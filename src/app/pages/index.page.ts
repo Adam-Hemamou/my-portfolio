c7d50a73ef5e107c550f8fcb50cfdbdb33a43a43
Adam HEMAMOU <h-mamou@hotmail.fr>
2025-05-13 09:14:03 +0200

    Start Project

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -4,17 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   standalone: true,
   template: `
-    <h2>Analog</h2>
-
-    <h3>The fullstack meta-framework for Angular!</h3>
-
-    <p class="read-the-docs">
-      <a href="https://analogjs.org" target="_blank">Docs</a> |
-      <a href="https://github.com/analogjs/analog" target="_blank">GitHub</a> |
-      <a href="https://github.com/sponsors/brandonroberts" target="_blank">
-        Sponsor
-      </a>
-    </p>
+    <h1>Bienvenue sur mon portfolio</h1>
+    <p>Je suis Adam, développeur Angular & Java.</p>
   `,
   styles: `
     :host {
@@ -25,7 +16,7 @@ import { Component } from '@angular/core';
     }
 
     .read-the-docs > * {
-      color: #fff;
+      color: red;
     }
 
     @media (prefers-color-scheme: light) {
