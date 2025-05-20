251d0e45e04454efbb18058fe033902a795e5bc1
Adam HEMAMOU <h-mamou@hotmail.fr>
2025-05-20 10:34:33 +0200

    Downgraded @analogjs dependencies to version 1.16.0 for compatibility. - Added new SVG icons for menu and arrow. - Refactored app.config.ts to remove client hydration. - Integrated a new MenuComponent in index.page.ts with a responsive menu. - Configured Vite to support SCSS inline styles.

---
+++ src/app/shared/menu/menu.component.ts
@@ -0,0 +1,28 @@
+import { NgFor, NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  imports: [NgIf, NgFor, RouterLink],
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss'],
+})
+export class MenuComponent {
+  isOpen = false;
+
+  menuItems = [
+    { label: 'Accueil', link: '/' },
+    { label: 'À propos', link: '/about' },
+    { label: 'Contact', link: '/contact' },
+    { label: 'Projets', link: '/project' },
+  ];
+
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+  }
+
+  closeMenu() {
+    this.isOpen = false;
+  }
+}
