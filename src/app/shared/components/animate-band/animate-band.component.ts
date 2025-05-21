f5a568cdbd1aa9af0a0e185d960ee32bb17ce250
Adam HEMAMOU <h-mamou@hotmail.fr>
2025-05-21 15:56:45 +0200

    Add footer component and styles;

--- src/app/shared/components/animate-band/animate-band.component.ts
+++ src/app/shared/components/animate-band/animate-band.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-animate-band',
+  standalone: true,
   imports: [],
   templateUrl: './animate-band.component.html',
   styleUrls: ['./animate-band.component.scss'],
--- src/app/shared/dump-components/project-card/project-card.component.ts
+++ src/app/shared/dump-components/project-card/project-card.component.ts
@@ -3,6 +3,7 @@ import { Project } from '../../models/project.type';
 
 @Component({
   selector: 'app-project-card',
+  standalone: true,
   imports: [],
   templateUrl: './project-card.component.html',
   styleUrls: ['./project-card.component.scss'],
