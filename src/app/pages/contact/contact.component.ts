850ae815f2dd61084fa073b35c2c9cc35057d15f
Adam HEMAMOU <h-mamou@hotmail.fr>
2025-05-23 21:26:36 +0200

    feat: implement contact page layout with hero section and footer components

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../../shared/components/menu/menu.component';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { AnimateBandComponent } from '../../shared/components/animate-band/animate-band.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent, AnimateBandComponent, FooterComponent],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
 })
