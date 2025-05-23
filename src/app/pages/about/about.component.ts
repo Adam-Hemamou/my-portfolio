ef038ae4eaefc8f1743fef6910a892fe9d67c3be
Adam HEMAMOU <h-mamou@hotmail.fr>
2025-05-23 11:16:53 +0200

    Update About page layout and styles; add content and new image

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { MenuComponent } from '../../shared/components/menu/menu.component';
+import { AnimateBandComponent } from '../../shared/components/animate-band/animate-band.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent, AnimateBandComponent, FooterComponent],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
