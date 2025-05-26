bbcec5dcc8c6e5de32ae48ba397c3cd3a5d58046
Adam HEMAMOU <h-mamou@hotmail.fr>
2025-05-26 09:53:50 +0200

    feat: create project list component with layout, styles, and sample data

--- src/app/pages/projects/project-list/project-list.component.ts
+++ src/app/pages/projects/project-list/project-list.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import { AnimateBandComponent } from '../../../shared/components/animate-band/animate-band.component';
+import { MenuComponent } from '../../../shared/components/menu/menu.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
+import { ProjectCardComponent } from '../../../shared/dump-components/project-card/project-card.component';
+import { Project } from 'src/app/shared/models/project.type';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-project-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor,
+    MenuComponent,
+    AnimateBandComponent,
+    ProjectCardComponent,
+    FooterComponent,
+  ],
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.scss'],
 })
-export class ProjectListComponent {}
+export class ProjectListComponent {
+  projects: Project[] = [
+    {
+      image: '/project-img/test.png',
+      label: 'WEBSITE',
+      title: 'ONDEO',
+      subtitle: 'Agence créative',
+      link: 'https://tonlien.com',
+    },
+    {
+      image: '/project-img/test.png',
+      label: 'JEU',
+      title: 'WAW',
+      subtitle: 'The Game',
+      link: 'https://tonlien.com',
+    },
+    {
+      image: '/project-img/test.png',
+      label: 'JEU',
+      title: 'WAW',
+      subtitle: 'The Game',
+      link: 'https://tonlien.com',
+    },
+    {
+      image: '/project-img/test.png',
+      label: 'JEU',
+      title: 'WAW',
+      subtitle: 'The Game',
+      link: 'https://tonlien.com',
+    },
+    {
+      image: '/project-img/test.png',
+      label: 'JEU',
+      title: 'WAW',
+      subtitle: 'The Game',
+      link: 'https://tonlien.com',
+    },
+    {
+      image: '/project-img/test.png',
+      label: 'JEU',
+      title: 'WAW',
+      subtitle: 'The Game',
+      link: 'https://tonlien.com',
+    },
+  ];
+}
